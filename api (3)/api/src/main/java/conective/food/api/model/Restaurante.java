f87019739728fbb5cb3b2c6e861b7d94cdcfefa8
Reprogramado <55607087+Reprogramado@users.noreply.github.com>
2023-06-07 15:45:31 -0300

    add docker file / add migrations / add models

---
+++ api (3)/api/src/main/java/conective/food/api/model/Restaurante.java
@@ -0,0 +1,46 @@
+package conective.food.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import jakarta.persistence.*;
+@Entity
+@Table(name = "restaurante")
+public class Restaurante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "endereco")
+    private String endereco;
+
+    // getters e setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
